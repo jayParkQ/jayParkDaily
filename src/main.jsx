d5e7136f014398befcc427adc7f2626aab87221e
jayParkQ <caoquynhchi2511@gmail.com>
2026-05-26 01:00:50 +0900

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FlowPlan from './FlowPlan'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FlowPlan />
+  </React.StrictMode>
+)
